1e1124b3bad4d9042a8b90b3041fae0549fce595
Afif Dev <afif_882003@yahoo.com>
2022-07-17 20:57:24 +0800

    Initial commit

--- server/GoogleServiceAccount.js
+++ server/GoogleServiceAccount.js
@@ -2,7 +2,7 @@ const serviceAccount = {
   type: process.env.GS_TYPE,
   project_id: process.env.GS_PROJECT_ID,
   private_key_id: process.env.GS_PRIVATE_KEY_ID,
-  private_key: process.env.GS_PRIVATE_KEY,
+  private_key: process.env.GS_PRIVATE_KEY.replace(/\\n/g, "\n"),
   client_email: process.env.GS_CLIENT_EMAIL,
   client_id: process.env.GS_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
