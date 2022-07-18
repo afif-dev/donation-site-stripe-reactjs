4b853bcc4b221c8c31c1a93d5901460f16abc24f
Afif Dev <afif_882003@yahoo.com>
2022-07-18 17:34:14 +0800

    enhanced donation form

--- client/src/components/DonationForm.jsx
+++ client/src/components/DonationForm.jsx
@@ -25,6 +25,7 @@ function DonationForm() {
   const [amount, setAmount] = useState(default_amount.toFixed(2));
   const [isOther, setOther] = useState(false);
   const [paymentType, setPaymentType] = useState(payment_types[0]);
+  const [isSubmit, setSubmit] = useState(false);
 
   const setDonationAmount = (am) => {
     if (am < min_amount) {
@@ -41,6 +42,7 @@ function DonationForm() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setSubmit(true);
 
     let data = new FormData();
     data.append("amount", amount);
@@ -123,10 +125,20 @@ function DonationForm() {
           <div className="basis-full p-2">
             <input type="hidden" name="mode" value={paymentType.mode} />
             <input type="hidden" name="interval" value={paymentType.interval} />
-            <button type="submit" className="block w-full p-8 bg-purple-700 text-white font-medium text-lg leading-tight uppercase rounded shadow-md hover:bg-purple-800 hover:shadow-lg focus:bg-purple-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-900 active:shadow-lg transition duration-150 ease-in-out">
-              Donate {currency_sign}
-              {amount}/{paymentType.name}
-            </button>
+            {isSubmit ? (
+              <button type="submit" className="block w-full p-8 bg-purple-800 text-white font-medium text-lg leading-tight uppercase rounded shadow-md" disabled>
+                <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white inline-block align-top" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                  <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                Processing...
+              </button>
+            ) : (
+              <button type="submit" className="block w-full p-8 bg-purple-700 text-white font-medium text-lg leading-tight uppercase rounded shadow-md hover:bg-purple-800 hover:shadow-lg focus:bg-purple-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-900 active:shadow-lg transition duration-150 ease-in-out">
+                Donate {currency_sign}
+                {amount}/{paymentType.name}
+              </button>
+            )}
           </div>
         </div>
       </form>
